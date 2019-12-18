c8d8fc08bd847327ef10cf2b9f8f4b81dfcf605e
abdulkader <abdulkader@ss-solutions.net>
2019-12-19 01:41:30 +0200

    set allowed hosts

--- mws_shopping/settings.py
+++ mws_shopping/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '&x+sb-w)!$dv^ys8lvkb236yc1-vkwwm$#_joj9n$^lte)%x@j'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'akhateeb.pythonanywhere.com', 'localhost']
 
 
 # Application definition
